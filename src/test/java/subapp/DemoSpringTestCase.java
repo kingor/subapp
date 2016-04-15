507f21d00e46c05333796ec19ca44aea40eb1e97
kingor <kingor333@mail.ru>
2016-04-15 16:55:53 +0300

    config files for test were created

--- src/test/java/subapp/DemoSpringTestCase.java
+++ src/test/java/subapp/DemoSpringTestCase.java
@@ -1,6 +1,6 @@
 package subapp;
 
-import junit.framework.TestCase;
+import static org.junit.Assert.assertNotNull;
 
 import org.junit.Ignore;
 import org.junit.Test;
@@ -12,17 +12,43 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import by.telecom.subapp.dao.SubscriberDao;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "src/main/webapp/WEB-INF/applicationContext.xml" })
-public class DemoSpringTestCase extends TestCase {
+@ContextConfiguration(locations = { "./applicationContext-test.xml" })
+public class DemoSpringTestCase {
 
 	@Autowired
 	SubscriberDao subscriberDao;
 
+	// @Autowired
+	// PhoneDao phoneDao;
+	// @Autowired
+	// UserDao userDao;
+	// @Autowired
+	// LogDao logDao;
+	//
+
 	@Ignore
 	@Test
-	public void testInjectBlogDao() throws Exception {
+	public void testInjectSubscriberDao() throws Exception {
 		System.out.println("SubscriberDao = " + subscriberDao);
 		assertNotNull(subscriberDao);
 	}
+	//
+	// @Test
+	// public void testInjectPhoneDao() throws Exception {
+	// System.out.println("PhoneDao = " + phoneDao);
+	// assertNotNull(phoneDao);
+	// }
+	//
+	// @Test
+	// public void testInjectUserDao() throws Exception {
+	// System.out.println("UserDao = " + userDao);
+	// assertNotNull(userDao);
+	// }
+	//
+	// @Test
+	// public void testInjectLogDao() throws Exception {
+	// System.out.println("LogDao = " + logDao);
+	// assertNotNull(logDao);
+	// }
 
 }
